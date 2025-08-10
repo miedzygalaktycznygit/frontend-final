3edcfc6a3eacff817f09ce27657485151a8c4d23
miedzygalaktycznygit <zamichal.789@gmail.com>
2025-08-11 01:25:12 +0200

    Konfiguruję vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+// Stary middleware do typu MIME jest już niepotrzebny, wtyczka PWA sobie z tym poradzi.
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // Konfiguracja wtyczki PWA
+    VitePWA({
+      // Używamy strategii 'injectManifest', która bierze nasz istniejący plik Service Workera
+      strategies: 'injectManifest',
+      // Wskazujemy, gdzie znajduje się nasz plik źródłowy
+      srcDir: 'public',
+      filename: 'firebase-messaging-sw.js',
+      // Wyłączamy domyślną rejestrację, ponieważ Firebase robi to za nas
+      injectRegister: null,
+    })
+  ],
+});
